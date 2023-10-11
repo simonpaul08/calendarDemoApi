f4a6677a713404fa20c476a2ee1af2b7f1c5f3c7
Simon Paul <samonpaul06@gmail.com>
2023-10-11 23:14:18 +0530

    cors option

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 
 const allowedOrigins = [
     "http://localhost:3000",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://calendar-demo-beta.vercel.app"
 ]
 
 export default allowedOrigins;
